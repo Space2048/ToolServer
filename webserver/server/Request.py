f439ebb2b7c6058a490a07fac680d37ca2b2ee2d
root <root@DESKTOP-D1Q3P06.localdomain>
2023-05-30 19:02:37 +0800

    get gps

--- webserver/server/Request.py
+++ webserver/server/Request.py
@@ -1,4 +1,8 @@
 
 class Request(object):
-    def __init__(self):
+    def __init__(self, handerObj):
+        self.remote_addr = handerObj.client_address
+        self.url_param = handerObj.url_param
+        self.headers = handerObj.headers
+        self.path = handerObj.path
         pass
