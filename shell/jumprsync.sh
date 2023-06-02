7af2a39617e849677b68c44f99b3238e3e6a7b78
root <root@DESKTOP-D1Q3P06.localdomain>
2023-06-02 18:31:15 +0800

    rm se

--- shell/jumprsync.sh
+++
@@ -1,9 +0,0 @@
-version=$1
-platform=$2
-packageName=$1"_"$2".zip"
-jumpDir="/isekai2_upload/"
-
-echo "sendFrom 124.156.139.110"
-echo "[SENDPACKAGE] |"${packageName}"| DATE |"`date`"|" >> client_package.log
-rsync --progress -e 'ssh  -oStrictHostKeyChecking=no -i ~/.ssh/id_rsa' ${packageName} root@35.194.91.23:${jumpDir};
-ssh root@35.194.91.23 "cd ${jumpDir}; sh sendPackage.sh $1 $2"
